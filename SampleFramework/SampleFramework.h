708785d632f2c4d2aed057e799c9d7dded1881f9
Zhu Jiang <scorpiozj@gmail.com>
2017-01-04 22:33:36 +0800

    Add init add: SampleFramework

---
+++ SampleFramework/SampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by ZhuJiang on 17/1/4.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
