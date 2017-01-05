34d194bd4a9b43dee5768d0113c18db12d6c58a6
Zhu Jiang <scorpiozj@gmail.com>
2017-01-05 10:29:08 +0800

    Add init add test files

---
+++ BInSample.swift
@@ -0,0 +1,13 @@
+//
+//  BInSample.swift
+//  SwiftAccessControlSample
+//
+//  Created by ZhuJiang on 17/1/5.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+import Foundation
+
+class TestAClass2 {
+    
+}
